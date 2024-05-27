3cd98b7a8e30c6f59d958be1b6ef4d2ead5e6dcb
Manuel Francisco <ciesafrancisco@hotmail.com>
2024-05-27 18:39:10 +0200

    last commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
- 
+let containerWrapperElemant = document.querySelector(".container-wrapper");
+let containerThankYouElement = document.querySelector(".container-thank-you");
+let btnSubmitElement = document.getElementById("submit");
+
+
+
+btnSubmitElement.addEventListener("click", () => {
+  console.log("clicked");
+  containerThankYouElement.classList.remove("hidden");
+  containerWrapperElemant.style.display = "none";
+});
